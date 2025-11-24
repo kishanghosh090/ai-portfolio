c5704a1f1c689b5208459177e69a165091f2f2f2
kishanghosh090 <kishanghosh090@gmail.com>
2025-11-24 18:28:57 +0530

    added smooth scroll

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import Footer from "@/components/Footer";
 import BottomNav from "@/components/BottomNav";
 import TopNav from "@/components/TopNav";
+import SmoothScroll from "@/components/SmoothScroll";
 
 export const metadata: Metadata = {
   title: "Kishan Rana Ghosh | Portfolio",
@@ -17,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="scroll-smooth">
       <body className="bg-[#050509] text-zinc-100 overflow-x-hidden">
+        <SmoothScroll />
         <div className="min-h-screen flex flex-col">
           {/* <Navbar /> */}
           <TopNav />
