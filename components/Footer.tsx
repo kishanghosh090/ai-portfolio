3f5e538cfd3541aaa0d1a81d5574d03849316063
kishanghosh090 <kishanghosh090@gmail.com>
2025-11-24 18:58:59 +0530

    bug fix

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="border-t border-white/5 bg-black/60">
+    <footer className="border-t border-white/5 bg-black/60 pb-11">
       <div className="max-w-5xl mx-auto px-4 py-6 text-xs text-zinc-500 flex justify-between">
         <span>© {new Date().getFullYear()} Kishan Rana Ghosh</span>
         <span>Built with Next.js, Tailwind, Gemini & a lot of chai ☕</span>
